d9072e27deb796b7755165389845c06cd0e60417
Emnet Befkadu <emnbef456@gmail.com>
2025-09-22 20:25:42 +0300

    c

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,14 @@
+import { div } from "framer-motion/m"
+
+const Button: React.FC = () => {
+    return(
+        <div>
+            <form action="">
+                <input type="text" 
+                placeholder="Tilte"
+                 className="rounded-md"
+                />
+            </form>
+        </div>
+    )
+}
